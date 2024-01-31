4d20c5ed02625e628384b546c298cc38dbc4bd2e
LauraSanin20 <laurasanin20@gmail.com>
2024-01-31 17:11:59 -0500

    comentario de prueba

--- helpers/creacionTabla.py
+++ helpers/creacionTabla.py
@@ -36,3 +36,5 @@ def crearTabla(dataFrame,nombreTabla):
     # Escribir la tabla y generarla en el proyecto
     with open(rutaArchivo,"w",encoding='utf-8') as archivo:
         archivo.write(f"{encabezadoHTML}\n{archivoHTML}\n</body>\n</html>")
+
+    # Se adiciona encoding='utf-8' en la linea 37 para que reconozca las tildes en la tabla
